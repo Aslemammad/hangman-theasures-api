090f80568c8550a2df02f20a2c95f4575710608f
M. Bagher Abiat <zorofight94@gmail.com>
2020-08-16 22:34:08 +0430

    Initial commit

--- index.js
+++ index.js
@@ -4,24 +4,23 @@ const path = require('path');
 const thesaurus = require('thesaurus');
 const cors = require('cors');
 
-app.use(
-	cors()
-);
+app.use(cors());
+app.get('/',function(req,res) {
+	res.json({welcome:[]})
+})
 app.get('/:word', function(req, res) {
 	try {
-
-	const payload = { thesaurus: thesaurus.find(req.params.word) };
-	if (payload.thesaurus.includes('undefined') || payload.thesaurus.includes('undefinable')) payload.thesaurus = [];
-return	res.json(payload);
-
-	} catch(e) {
-res.json({thesaurus:[],error:'Bad error'})
+		const payload = { thesaurus: thesaurus.find(req.params.word) };
+		if (payload.thesaurus.includes('undefined') || payload.thesaurus.includes('undefinable'))
+			payload.thesaurus = [];
+		return res.json(payload);
+	} catch (e) {
+		res.json({ thesaurus: [], error: 'Bad error' });
 	}
-
 });
-app.get('/*',function(req,res) {
-	res.json({thesaurus:[]})
-})
+app.get('/*', function(req, res) {
+	res.json({ thesaurus: [] });
+});
 app.listen(process.env.PORT || 4000, function() {
 	console.log('Node app is working!', process.env.PORT);
 });
