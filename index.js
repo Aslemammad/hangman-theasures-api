b3b1204bbe8866d242a9fb17eb3308537dd98eb7
M. Bagher Abiat <zorofight94@gmail.com>
2020-08-16 22:22:27 +0430

    Initial commit

--- index.js
+++ index.js
@@ -4,22 +4,8 @@ const path = require('path');
 const thesaurus = require('thesaurus');
 const cors = require('cors');
 
-const whitleListDomain = [
-	'http://localhost:5005',
-	'https://aslemammad-hangman.surge.com'
-];
-
 app.use(
-	cors({
-		origin : function(origin, callback) {
-			if (!origin) return callback(null, true);
-			if (whitleListDomain.indexOf(origin) === -1) {
-				var msg = 'The CORS policy for this site does not ' + 'allow access from the specified Origin.';
-				return callback(new Error(msg), false);
-			}
-			return callback(null, true);
-		}
-	})
+	cors()
 );
 app.get('/:word', function(req, res) {
 	const payload = { thesaurus: thesaurus.find(req.params.word) };
